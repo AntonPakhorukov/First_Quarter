f942efff346148268937e8006dea7624df2102d1
ÄntonPakhorukov <pakhorukov.a@mail.ru>
2022-11-16 23:05:31 +0700

    Array

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.Clear();
+Console.Write("Введите размер массива: ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[sizeArray];
+string[] GetArray(string[] inputArray) {
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        inputArray[i] = Console.ReadLine();
+    }
+    return inputArray;
+}
+array = GetArray(array);
+Console.WriteLine("[" + String.Join(", ", array) + "]");
