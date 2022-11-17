1d01d2925e55a0086a03638084652fd6b2226ade
ÄntonPakhorukov <pakhorukov.a@mail.ru>
2022-11-17 16:12:52 +0700

    first branch switch

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,13 +1,37 @@
 ﻿Console.Clear();
-Console.Write("Введите размер массива: ");
-int sizeArray = Convert.ToInt32(Console.ReadLine());
-string[] array = new string[sizeArray];
-string[] GetArray(string[] inputArray) {
-    for (int i = 0; i < inputArray.Length; i++)
-    {
-        inputArray[i] = Console.ReadLine();
-    }
-    return inputArray;
+Console.Write("Вы хотите использовать исходный массив(1) или задать вручную(2)?");
+int solution = Convert.ToInt32(Console.ReadLine());
+switch (solution)
+{
+    case 1:
+        Solution1();
+        break;
+    case 2:
+        Solution2();
+        break;
 }
-array = GetArray(array);
-Console.WriteLine("[" + String.Join(", ", array) + "]");
+void Solution1()
+{
+    string[] array = { "Hello", "all", "world", "we", "good", "woman", "and", "man" };
+    Console.WriteLine("[" + String.Join(", ", array) + "]");
+}
+void Solution2()
+{
+    Console.Write("Введите размер массива: ");
+    int sizeArray = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[sizeArray];
+    Console.WriteLine("Введите данные массива:");
+    string[] GetArray(string[] inputArray)
+    {
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            inputArray[i] = Console.ReadLine();
+        }
+        return inputArray;
+    }
+    array = GetArray(array);
+    Console.Clear();
+    Console.WriteLine("[" + String.Join(", ", array) + "]");
+}
+
+
